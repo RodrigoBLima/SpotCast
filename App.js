4d31b71c691f5a24f6aa1aa9c5ccfd92200c4637
Rodrigo Barbosa <rodrigo.lima@agriconnected.com>
2020-09-08 23:08:48 -0300

    routes

--- App.js
+++ App.js
@@ -1,22 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import PlayerScreen from './src/screens/Player';
+import React from "react";
+import PlayerScreen from "./src/screens/Player";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { View, Text, TouchableOpacity } from "react-native";
+import { StatusBar } from "expo-status-bar";
+// import {TouchableOpacity} from 'react-native-gesture-handler'
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <PlayerScreen />
+//     // <View style={styles.container}>
+//     //   <Text>Open up App.js to start working on your app!</Text>
+//     //   <StatusBar style="auto" />
+//     // </View>
+//   );
+// }
+function HomeScreen(props) {
+  const { navigate } = props.navigation;
   return (
-    <PlayerScreen />
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
+    <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+      <Text>Home Screen</Text>
+      {[1, 2, 3, 4, 5].map((item) => {
+        return (
+          <TouchableOpacity
+            onPress={() => {
+              navigate("PlayerScreen");
+            }}
+            key={item}
+          >
+            <Text>Podcast: {item}</Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+}
+function DiscoveryScreen() {
+  return (
+    <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+      <Text>Discovery Screen</Text>
+    </View>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+// =====================  ROUTES  =====================
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="HomeScreen" component={PodcastsTabStack} />
+
+        <Tab.Screen name="DiscoveryScreen" component={DiscoveryScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const PodcastsStack = createStackNavigator();
+
+function PodcastsTabStack() {
+  return (
+    <PodcastsStack.Navigator>
+      <PodcastsStack.Screen name="HomeScreen" component={HomeScreen} />
+      <PodcastsStack.Screen name="PlayerScreen" component={PlayerScreen} />
+    </PodcastsStack.Navigator>
+  );
+}
